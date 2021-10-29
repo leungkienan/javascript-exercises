8e87a6ea0384c9b5607a8fa76c334120814f0de6
Kienan <leung.kienan@gmail.com>
2021-10-29 14:56:24 +1300

    completed exercise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let strings = [];
+  console.log(str)
+  if (num >= 1) {
+    for (let i = 0; i < num; i++) {
+      strings.push(str);
+    }
+    return strings.join("");
+    // console.log(strings.join(""))
+  } else if ((num = 0)) {
+    return "";
+  } else if(num<0){
+      return 'ERROR'
+  }
 };
 
 // Do not edit below this line
