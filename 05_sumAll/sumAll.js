e57d5958818eb69b927da62b92085c01c88a4b26
kienan <leung.kienan@gmail.com>
2021-12-05 17:47:00 +1300

    completed upto ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,52 @@
 const sumAll = function(min, max) {
-    if()
-        var n = (max - min + 1);
-        var sum = n * (max + min) / 2; // note integer division
-    
-        return sum;
+
+    // function add(acc,a){
+    //     return acc + a
+    // }
+
+    // // console.log("this is min: "+min)
+    // // console.log("this is max: "+max)
+    // if(min<0 || max<0 || min==NaN || max==NaN){
+    //     return 'ERROR'
+    // } else if(min>max){
+
+    //     let maxSum=max;
+    //     maxArr=[]
+    //     for(let i=0; i<min; i++){
+    //         maxArr.push(maxSum)
+    //         maxSum = max+1
+    //         // console.log("this is max sum: "+maxSum)
+    //     }
+    //     let sum = maxArr.reduce(add, 0)
+    //     return sum;
+    // } else{
+    //     let minSum=min;
+    //     let minArr =[]
+    //     for(let n=0; n<max; n++){
+    //         minArr.push(minSum)
+    //         minSum=min+1;
+    //         // console.log("this is min sum: "+minSum)
+    //     }
+    //     let sum = minArr.reduce(add, 0)
+    //     console.log("sum is: "+sum)
+    //     console.log("minArr is: "+minArr)
+    //     return sum
+    //  }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+      sum += i;
+    }
+    return sum;
+
+
 };
 
 // Do not edit below this line
