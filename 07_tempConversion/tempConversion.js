679234474b400224c074398f0f98f9ffe250c707
Kienan Leung <leung.kienan@gmail.com>
2022-06-18 11:23:00 +1200

    complete half ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(n) {
+  let convForm = 5/9; 
+  let i = (n-32)*convForm;
+  let conv = Math.round(i*10)/10
+  return conv
 };
 
-const ctof = function() {
+const ctof = function(n) {
 
 };
 
