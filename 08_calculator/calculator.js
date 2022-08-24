0db53f69b85dff06c3df2c20a09a5be01dc52d76
Kienan Leung <leung.kienan@gmail.com>
2022-08-24 16:16:14 +1200

    completed js exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let i = arr.reduce((n, a)=> n+a,0)
+  return i 
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let i = arr.reduce((a,b)=> a*b,1)
+  return i
+};
+
+const power = function(num, pow) {
+  let calc = Math.pow(num, pow)
+  return calc
+  // return sum
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  let ans = 1;
+  if(n==0 || n==1){
+    return ans
+  } else{
+    return n*factorial(n-1)
+  }
 	
 };
 
